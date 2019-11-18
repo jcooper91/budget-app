b045963e41cd94175d10e37675f5e5c81d01c1ff
JCooper91 <jackcooper246@gmail.com>
2019-11-18 21:52:03 +0000

    Setup error validation for budget form

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,57 @@
 import '../styles/styles.css'
-import { Budget } from './budget'
+import { timingSafeEqual } from 'crypto';
+// 29 mins
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-amount");
+    this.expenseAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+  }
 
-document.getElementById('budget-form').addEventListener('submit', Budget.getBudget())
+  submitBudgetForm() {
+    const value = this.budgetInput.value 
+    if(value === '' || value < 0) {
+      this.budgetFeedback.classList.add('showItem')
+      this.budgetFeedback.innerHTML = `<p>Budget cannot be empty or negative number</p>`
+      setTimeout(() => {
+       this.budgetFeedback.classList.remove('showItem')
+      }, 2000)
+    }
+  }
+}
+
+function eventListeners() {
+  const budgetForm = document.getElementById("budget-form")
+  const expenseForm = document.getElementById("expense-form")
+  const expenseList = document.getElementById("expense-list")
+  
+  const ui = new UI()
+
+  budgetForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    ui.submitBudgetForm()
+  })
+
+  expenseForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
+
+  expenseList.addEventListener('click', (e) => {
+
+  })
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  eventListeners()
+})
